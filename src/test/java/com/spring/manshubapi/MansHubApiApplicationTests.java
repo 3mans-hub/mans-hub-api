709cbc1968331aa383d39091e429d5c7a720e556
kkimjuseong <rofodzl1@naver.com>
2024-09-20 14:29:31 +0900

    # initial commit

---
+++ src/test/java/com/spring/manshubapi/MansHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.manshubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MansHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
