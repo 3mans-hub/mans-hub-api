8eaecc14e1cf09cd9352497a41828aa93a242ef1
kkimjuseong <rofodzl1@naver.com>
2024-09-28 21:42:20 +0900

    # 챗 메세지 엔터티

--- src/main/java/com/spring/manshubapi/entity/ChatMessage.java
+++ src/main/java/com/spring/manshubapi/entity/ChatMessage.java
@@ -1,6 +1,7 @@
 package com.spring.manshubapi.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -23,20 +24,29 @@ public class ChatMessage {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private String messageId;
 
+    @Setter
     @Column(name = "message_content")
     private String content;
 
+    @Setter
     @Column(name = "create_at")
     private LocalDateTime createAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    @JsonBackReference("message-user")
+//    @JsonBackReference("message-user")
+    @Setter
+    @JsonIgnore
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
-    @JsonBackReference("message-team")
+//    @JsonBackReference("message-team")
+    @JsonIgnore
     private Team team;
 
+    @PrePersist
+    public void prePersist() {
+        this.createAt = this.createAt == null ? LocalDateTime.now() : this.createAt;
+    }
 }
