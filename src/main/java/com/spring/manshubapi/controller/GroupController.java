a87b2b613f8d8c1c8e52aa5c24c4565a964d2d74
JinWooP98 <qwdk0406@naver.com>
2024-10-30 23:25:35 +0900

    그룹 생성 및 참여하고 있는 그룹 조회 기능 추가

---
+++ src/main/java/com/spring/manshubapi/controller/GroupController.java
@@ -0,0 +1,37 @@
+package com.spring.manshubapi.controller;
+
+import com.spring.manshubapi.dto.response.CreateGroupResponseDto;
+import com.spring.manshubapi.dto.response.FindGroupResponseDto;
+import com.spring.manshubapi.entity.Team;
+import com.spring.manshubapi.service.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/group")
+@RequiredArgsConstructor
+public class GroupController {
+
+    private final GroupService groupService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createGroup(@RequestBody CreateGroupResponseDto createGroupResponseDto) {
+
+        Team newGroup = groupService.createGroup(createGroupResponseDto);
+
+
+        return ResponseEntity.ok().body(newGroup);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<?> findGroupList(@RequestBody FindGroupResponseDto findGroupResponseDto) {
+
+        List<Team> teamList = groupService.findGroup(findGroupResponseDto);
+
+        return ResponseEntity.ok().body(teamList);
+    }
+}
