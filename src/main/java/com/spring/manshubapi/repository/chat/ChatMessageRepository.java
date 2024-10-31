dc1fad366acb65ac239d6628571378579dbc9743
kkimjuseong <rofodzl1@naver.com>
2024-10-31 16:19:34 +0900

    # 채팅레파지토리에서 팀아이디 불러오기

--- src/main/java/com/spring/manshubapi/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/spring/manshubapi/repository/chat/ChatMessageRepository.java
@@ -3,7 +3,9 @@ package com.spring.manshubapi.repository.chat;
 import com.spring.manshubapi.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-
+    List<ChatMessage> findByTeam_TeamId(String teamId);
 }
