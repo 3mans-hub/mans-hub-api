2f4a6f0be9a4094f5f06ceae41ebd7f2693c0fc0
kkimjuseong <rofodzl1@naver.com>
2024-10-31 17:12:06 +0900

    # 채팅 컨트롤러 수정

--- src/main/java/com/spring/manshubapi/controller/ChatController.java
+++ src/main/java/com/spring/manshubapi/controller/ChatController.java
@@ -31,46 +31,39 @@ public class ChatController {
     private final TeamRepository teamRepository;
 
     // 클라이언트가 "/app/sendMessage"로 메시지를 보낼 때 처리
-
-
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
     public ChatMessageResponseDto broadcastMessage(@Payload ChatMessageDto messageDto) {
-        // DateTimeFormatter 를 사용하여 시간 파싱
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-
         User userId = userRepository.findById(messageDto.getUserId())
                 .orElseThrow(() -> new RuntimeException("User not found"));
+        Team teamId = teamRepository.findById(messageDto.getTeamId())
+                .orElseThrow(() -> new RuntimeException("Team not found"));
 
-        Team teamId = teamRepository.findById(messageDto.getTeamId()).orElseThrow(() -> new RuntimeException("Team not found"));
-
-        // ChatMessage 엔티티 생성 및 저장
+        // ChatMessage 엔티티 생성 (createAt은 @PrePersist로 자동 설정)
         ChatMessage messageEntity = ChatMessage.builder()
                 .content(messageDto.getContent())
-                .createAt(LocalDateTime.parse(messageDto.getCreateAt(), formatter))
                 .user(userId)
                 .team(teamId)
                 .build();
 
         chatMessageRepository.save(messageEntity);
 
-        // 응답 DTO 반환
-        return new ChatMessageResponseDto(userId.getName(), messageDto.getContent(), messageDto.getCreateAt());
+        return new ChatMessageResponseDto(userId.getName(), messageDto.getContent(), messageEntity.getCreateAt().toString());
     }
+
 
     // 채팅방에 입장할 때, 이전 채팅 기록을 불러오기
     @MessageMapping("/loadMessages")
     @SendTo("/topic/public")
-    @Transactional  // 트랜잭션 보장
-    public List<ChatMessageResponseDto> loadPreviousMessages() {
-        // DB에 저장된 이전 메시지들을 불러오기 (엔티티 -> DTO 변환)
-        return chatMessageRepository.findAll()
+    @Transactional
+    public List<ChatMessageResponseDto> loadPreviousMessages(@Payload ChatMessageDto messageDto) {
+
+        return chatMessageRepository.findByTeam_TeamId(messageDto.getTeamId())
                 .stream()
-                .map(entity -> new ChatMessageResponseDto(
-                        entity.getUser() != null ? entity.getUser().getName() : "Unknown User",
+                .map(entity -> new ChatMessageResponseDto(entity.getUser() != null ? entity.getUser().getName() : "Unknown User",
                         entity.getContent(), entity.getCreateAt().toString()
                 ))
                 .collect(Collectors.toList());
-    }
 
+    }
 }
