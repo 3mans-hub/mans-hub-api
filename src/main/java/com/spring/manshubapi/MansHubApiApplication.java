9b35e1cd4a15a2f5511f063eb93975e9087ce946
kkimjuseong <rofodzl1@naver.com>
2024-11-04 15:36:29 +0900

    # 톰캣로그

--- src/main/java/com/spring/manshubapi/MansHubApiApplication.java
+++ src/main/java/com/spring/manshubapi/MansHubApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MansHubApiApplication {
 
     public static void main(String[] args) {
-        System.out.println("1031 빌드 테스트");
+        System.out.println("1104 빌드 테스트");
         SpringApplication.run(MansHubApiApplication.class, args);
     }
 
